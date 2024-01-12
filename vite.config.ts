5dd932be498bef72297254140199b61448a880da
RobbyRecylink <roberto@recylink.com>
2024-01-12 16:00:30 -0300

    correccion de errores

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import { defineConfig, PluginOption } from 'vite'; // Import the PluginOption type
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()] as PluginOption[], // Cast the plugins array to PluginOption[]
   server: {
     port: 6001
   },
